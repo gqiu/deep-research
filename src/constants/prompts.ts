0e7b0a1a350b444887cf7365017e97e338b247cb
Gerry Qiu <qiulanfang@gmail.com>
2025-06-22 16:15:05 +0800

    update

--- src/constants/prompts.ts
+++ src/constants/prompts.ts
@@ -199,7 +199,7 @@ export const finalReportReferencesPrompt = `Citation Rules:
 - Please cite research references at the end of your paragraphs when appropriate.
 - If the citation is from the reference, please **ignore**. Include only references from sources.
 - Please use the reference format [number], to reference the learnings link in corresponding parts of your answer.
-- If a paragraphs comes from multiple learnings reference link, please list all relevant citation numbers, **e.g., [1][2]**. Remember not to group citations at the end but list them in the corresponding parts of your answer. Control the number of footnotes.
+- If a paragraphs comes from multiple learnings reference link, please list all relevant citation numbers, **e.g., [[1]][[2]]**. Remember not to group citations at the end but list them in the corresponding parts of your answer. Control the number of footnotes.
 - Do not have more than 3 reference link in a paragraph, and keep only the most relevant ones.
 - **Do not add references at the end of the report.**`;
 
